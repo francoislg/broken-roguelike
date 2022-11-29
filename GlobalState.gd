a845c68c2723af591289de868bdb95b6e992d5a5
francoislg <francois.lguillemette@gmail.com>
2022-11-29 14:11:00 -0500

    Add clock + thread the cpu wmi call

--- GlobalState.gd
+++ GlobalState.gd
@@ -1,13 +1,21 @@
 extends Node
 
 enum States {
-   CPU_USAGE,
-   TOTAL_RAM
+	CPU_USAGE,
+	TOTAL_RAM,
+	CLOCK_HOURS,
+	CLOCK_MINUTES,
+	CLOCK_SECONDS,
+	DAY_OF_MONTH
 };
 
 var state = {
 	States.CPU_USAGE: 0,
-	States.TOTAL_RAM: 0
+	States.TOTAL_RAM: 0,
+	States.CLOCK_HOURS: 0,
+	States.CLOCK_MINUTES: 0,
+	States.CLOCK_SECONDS: 0,
+	States.DAY_OF_MONTH: 0,
 }
 
 # Called when the node enters the scene tree for the first time.
@@ -22,7 +30,7 @@ func threadedUpdate():
 
 	var fastTimer = Timer.new()
 	add_child(fastTimer)
-	fastTimer.wait_time = 5
+	fastTimer.wait_time = 1
 	fastTimer.connect('timeout', fastUpdates);
 	fastTimer.start()
 
@@ -39,7 +47,9 @@ func slowUpdates():
 	return
 
 func fastUpdates():
-	updateCpuUsage()
+	var cpuUsageThread = Thread.new()
+	cpuUsageThread.start(updateCpuUsage)
+	updateClock()
 
 func updateCpuUsage():
 	var result = wmicCall("cpu get loadpercentage")
@@ -50,6 +60,13 @@ func updateTotalRam():
 	var ramInBytes = wmicCall("computersystem get totalphysicalmemory")
 	if ramInBytes:
 		state[States.TOTAL_RAM] = float(ramInBytes) / 1024 / 1024 / 1024
+		
+func updateClock():
+	var time = Time.get_datetime_dict_from_system()
+	state[States.CLOCK_HOURS] = time.hour
+	state[States.CLOCK_MINUTES] = time.minute
+	state[States.CLOCK_SECONDS] = time.second
+	state[States.DAY_OF_MONTH] = time.day
 
 # Could probably use one wmic call with "/value" to retrieve every PC information at once
 
