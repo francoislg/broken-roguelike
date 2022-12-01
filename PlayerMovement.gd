0efe7f914d577be0264eb4da904213c49b3dfcf9
francoislg <francois.lguillemette@gmail.com>
2022-12-01 13:38:06 -0500

    Add fastfall, keep jump to stay higher, add hitstun on attack

--- PlayerMovement.gd
+++ PlayerMovement.gd
@@ -8,32 +8,37 @@ const WALK_MAX_SPEED = 575
 const STOP_FORCE = 2000
 const JUMP_SPEED = 700
 const WALL_GRAVITY_MODIFIER = 0.25
-const ENEMY_HIT_KNOCKBACK_FORCE = 500
+const JUMP_BUTTON_GRAVITY_MODIFIER = 0.50
+const ENEMY_HIT_KNOCKBACK_FORCE = 700
 
-var gravity = 2000 #ProjectSettings.get_setting("physics/2d/default_gravity")
+var gravity = 3000 #ProjectSettings.get_setting("physics/2d/default_gravity")
 
-var recently_wall_jumped = false
+var movement_stopped = false
 var buffered_frames_jump = 0
 var jump_touched_a_wall = false
-var resetWallTimer := Timer.new()
+var lastJumpTimer := Timer.new()
+var movementStoppedTimer := Timer.new()
 
 func _ready():
-	add_child(resetWallTimer)
-	resetWallTimer.wait_time = 0.2
-	resetWallTimer.one_shot = true
-	resetWallTimer.connect("timeout", _on_timer_reset_wall_jump)
+	add_child(movementStoppedTimer)
+	movementStoppedTimer.wait_time = 0.2
+	movementStoppedTimer.one_shot = true
+	movementStoppedTimer.connect("timeout", _on_timer_movement_stopped)
 
 func _physics_process(delta):
 	if (Input.is_action_just_pressed("LEFT") || Input.is_action_just_pressed("RIGHT")) and is_on_floor():
 		velocity.x = velocity.x / 2
 	var walk = WALK_FORCE * (Input.get_action_strength("RIGHT") - Input.get_action_strength("LEFT"))
 	
+	if movement_stopped:
+		walk = 0
+	
 	if abs(walk) < WALK_FORCE * 0.2:
 		velocity.x = move_toward(velocity.x, 0, STOP_FORCE * delta)
 	else:
 		velocity.x += walk * delta
 	
-	#Stops most of the momentum if you press the opposite direction of the current velocity for better air control	
+	#Stops most of the momentum if you press the opposite direction of the current velocity for better air control
 	if walk < 0:
 		velocity.x = clamp(velocity.x, -WALK_MAX_SPEED, WALK_MAX_SPEED * 0.1)
 	elif walk > 0:
@@ -55,12 +60,17 @@ func _physics_process(delta):
 	if (touchesRight):
 		velocity.x = clamp(velocity.x, -WALK_MAX_SPEED, 0)
 	
+	var isGoingUp = velocity.y < 0;
+	
 	var baseGravity = gravity * delta
-	if (touchesAWall and not recently_wall_jumped):
+	if (touchesAWall and not movement_stopped):
 		velocity.y = clamp(velocity.y + baseGravity * WALL_GRAVITY_MODIFIER, -JUMP_SPEED * WALL_GRAVITY_MODIFIER, 10000)
 	else:
-		velocity.y += baseGravity
-		
+		velocity.y += baseGravity * JUMP_BUTTON_GRAVITY_MODIFIER if isGoingUp and Input.is_action_pressed("JUMP") else baseGravity
+	
+	if Input.is_action_just_pressed("FALL"):
+		velocity.y = JUMP_SPEED
+
 	if (Input.is_action_just_pressed("JUMP")):
 		buffered_frames_jump = 0.1
 
@@ -69,8 +79,8 @@ func _physics_process(delta):
 			buffered_frames_jump = 0
 			velocity.y = -JUMP_SPEED
 			if (touchesAWall):
-				resetWallTimer.start()
-				recently_wall_jumped = true
+				stopMovementFor(0.2)
+				movement_stopped = true
 				if (touchesLeft):
 					velocity.x = JUMP_SPEED
 				elif (touchesRight):
@@ -82,20 +92,22 @@ func _physics_process(delta):
 	
 	if velocity.x != 0:
 		sprite.set_flip_h(velocity.x < 0)
-		
 
-func _on_timer_reset_wall_jump():
-	recently_wall_jumped = false
+func stopMovementFor(time: int):
+	movementStoppedTimer.wait_time = time
+	movementStoppedTimer.start()
+	movement_stopped = true
 
+func _on_timer_movement_stopped():
+	movement_stopped = false
 
 func _on_attack_range_area_body_entered(enemyHit: BaseEnemy):
 	var bodies = attackRangeArea.get_overlapping_bodies()
 	var playerHitDirection = (enemyHit.position - position).normalized()
 	velocity = -playerHitDirection * ENEMY_HIT_KNOCKBACK_FORCE
+	stopMovementFor(0.2)
 	
 	for body in bodies:
 		if body is BaseEnemy:
 			var enemyHitDirection = (body.position - position).normalized()
 			body.receive_damage(enemyHitDirection, 1)
-	
-	pass # Replace with function body.
