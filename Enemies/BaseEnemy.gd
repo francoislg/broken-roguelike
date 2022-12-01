a4af4cee6421cc19e1d80f80061a413aa6554281
francoislg <francois.lguillemette@gmail.com>
2022-12-01 11:59:49 -0500

    Add hp bar to enemies

--- Enemies/BaseEnemy.gd
+++ Enemies/BaseEnemy.gd
@@ -2,16 +2,16 @@ extends CharacterBody2D
 
 class_name BaseEnemy
 
+@onready var health := $Health
 @onready var character: CharacterBody2D = $"/root/Scene/Character"
 
-var hp: float = 5
+var initialHp: float = 5
+var hp: float
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	hp = initialHp
+	update_progress_bar(1)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
@@ -21,3 +21,11 @@ func receive_damage(direction: Vector2, damage: float):
 		queue_free()
 	else:
 		velocity = direction * 200
+		update_progress_bar(hp / initialHp)
+		
+
+func update_progress_bar(ratio: float):
+	health.value = ratio * 100
+	var r = lerpf(1, 0, ratio)
+	var g = lerpf(0, 1, ratio)
+	health.set("modulate", Color(r, g, 0))
--- Enemies/FollowingEnemy.gd
+++ Enemies/FollowingEnemy.gd
@@ -4,12 +4,10 @@ const BASE_SPEED = 100
 # The higher this is, the faster the enemy will get to speed
 const TIME_TO_GET_TO_SPEED = 500
 
-func _ready():
-	pass
-
 func _physics_process(delta):
 	var vector = (character.position - position).normalized()
 	
 	velocity = velocity.move_toward(vector * BASE_SPEED, delta * TIME_TO_GET_TO_SPEED)
 	
 	move_and_collide(velocity * delta)
+
