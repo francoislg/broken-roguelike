40b7a8c6553223ee413c7c59ea73478fa4ce89f0
francoislg <francois.lguillemette@gmail.com>
2023-09-18 15:24:55 -0400

    Bring back DrawLineToRespawn

--- Enemies/Debug/DrawLineToRespawn.gd
+++ Enemies/Debug/DrawLineToRespawn.gd
@@ -8,26 +8,20 @@ var isParentSelected: bool = false
 
 func _ready():
 	if Engine.is_editor_hint():
-		parent = get_owner()
+		var selection: EditorSelection = EditorPlugin.new().get_editor_interface().get_selection()
+		selection.connect("selection_changed", on_selection_changed)
 
-# Doesn't work for now, wait until EditorInterface is a proper singleton
-#		var selection = EditorPlugin.new().get_editor_interface().get_selection()
-#		selection.connect("selection_changed", func():
-#			isParentSelected = parent.get_instance_id() in selection.get_selected_nodes().map(func(node): return node.get_instance_id())
-#
-#			if isParentSelected:
-#				check_respawner_prop()
-#
-#			queue_redraw()
-#		)
+func on_selection_changed():
+	parent = get_owner()
+	if parent:
+		var id = parent.get_instance_id();
+		var selection: EditorSelection = EditorPlugin.new().get_editor_interface().get_selection()
+		var nodes = selection.get_transformable_selected_nodes()
+		isParentSelected = id in nodes.map(func(node): return node.get_instance_id())
+		if isParentSelected:
+			respawner = parent.get("respawner")
 
-
-func check_respawner_prop():
-	if "_editor_prop_ptr_respawner" in parent.get_meta_list():
-		var respawnerPath = parent.get_meta("_editor_prop_ptr_respawner")
-		if respawnerPath:
-			respawner = parent.get_node(respawnerPath)
-
+		queue_redraw()
 
 func _process(_delta):
 	if Engine.is_editor_hint() and respawner != null and isParentSelected:
